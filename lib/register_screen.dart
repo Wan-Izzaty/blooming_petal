ed5f534e1b878f79f17b196269138abac314ed34
Yiieen <kangdijah13@gmail.com>
2026-05-23 22:57:04 +0800

    del

--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -1,4 +1,3 @@
-import 'package:blooming_petal_application/navigation_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
