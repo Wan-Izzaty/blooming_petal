2540509ef7c0c5825c70489493d14cf28e619405
Yiieen <kangdijah13@gmail.com>
2026-05-23 18:59:47 +0800

    main changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'navigation_screen.dart';
+import 'package:firebase_core/firebase_core.dart'; // tambah ni
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const BloomingPetalsApp());
 }
 
